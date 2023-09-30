10536b94509120c1c76a2363285f5ab0f998990d
AlejoGalizzi <alejogalizzi@gmail.com>
2023-09-30 19:32:17 -0300

    Initial commit with the starter project.

---
+++ E-CommerceWebParent/E-CommerceFrontend/src/test/java/com/alejogalizzi/ecommerceFrontend/ECommerceFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejogalizzi.ecommerceFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
